556a660973f29f980a90966c9a3cd1073a0c9e74
Kyle Pasag <kylepasag@gmail.com>
2026-01-17 13:47:12 -0800

    Created footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <h3>© MedTime 2026</h3>
+      <p>All rights reserved</p>
+      <p>Privacy Policy</p>
+      <p>Terms of Service</p>
+
+      
+    </footer>
+  );
+}
+
+export default Footer;
