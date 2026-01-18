844eb86c816e8777e3a13ca5e405f868f7420c87
Jameel Mohammed <jameeel01@gmail.com>
2026-01-17 19:07:07 -0800

    Update redundant code

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useEffect } from "react";
+import { useEffect, useState} from "react";
 import { auth } from '../../firebase.config'
 import { signOut } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
