1c6f93a57c44a99864c731aa41a64a1099f2c86c
Kyle Pasag <kylepasag@gmail.com>
2026-01-17 17:11:02 -0800

    Make header & footer links work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
-import './Header.css';
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="header">
       <h1 className="title">MedTime</h1>
+
       <nav className="nav">
-        <a href="#" className="link">Home</a>
-        <a href="#" className="link">About</a>
-        <a href="#" className="link">Contact</a>
-        <a href="#" className="link">Sign Up</a>
+        <Link to="/" className="link">Home</Link>
+        <Link to="/dashboard" className="link">Dashboard</Link>
+        <Link to="/login" className="link">Login</Link>
       </nav>
     </header>
   );
